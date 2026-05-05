19c786aa9830d81a7fb965d1087149637559df7f
viethoang1508 <15082004qwerty@gmail.com>
2026-05-05 10:48:16 +0700

    Cập nhật api-gateway

---
+++ api_gateway/src/main/java/org/example/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package org.example.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeExchange(exchange -> exchanges
+                        .pa)
+    }
+}
---
+++ api_gateway/src/main/java/org/example/api_gateway/filter/UserContextFilter.java
@@ -0,0 +1,36 @@
+package org.example.api_gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class UserContextFilter  implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return exchange.getPrincipal()
+                .filter(p -> p instanceof JwtAuthenticationToken)
+                .cast(JwtAuthenticationToken.class)
+                .flatMap(userId ->{
+                    ServerHttpRequest request = exchange.getRequest()
+                            .mutate()
+                            .header("X-User-Id", userId)
+                            .build();
+
+                    return chain.filter(
+                            exchange.mutate().request(request).build()
+                    );
+                })
+
+                .switchIfEmpty(chain.filter(exchange));
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
