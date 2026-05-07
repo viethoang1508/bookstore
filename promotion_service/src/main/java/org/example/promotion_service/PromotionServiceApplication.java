b64c931af4ae8bf57860eb0d7c1475eb03650523
viethoang1508 <15082004qwerty@gmail.com>
2026-05-07 10:43:41 +0700

    e

--- promotion_service/src/main/java/org/example/promotion_service/PromotionServiceApplication.java
+++ promotion_service/src/main/java/org/example/promotion_service/PromotionServiceApplication.java
@@ -2,7 +2,9 @@ package org.example.promotion_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 @SpringBootApplication
 public class PromotionServiceApplication {
 
