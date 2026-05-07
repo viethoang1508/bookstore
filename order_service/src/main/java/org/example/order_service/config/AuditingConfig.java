a6df4405a6624ab8debeb2ac7e26d44aa17c10c7
viethoang1508 <15082004qwerty@gmail.com>
2026-05-07 10:18:40 +0700

    x

---
+++ order_service/src/main/java/org/example/order_service/config/AuditingConfig.java
@@ -0,0 +1,23 @@
+package org.example.order_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+public class AuditingConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAware<String>() {
+            @Override
+            public Optional<String> getCurrentAuditor() {
+                return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                        .map(Authentication::getName);
+            }
+        };
+    }
+}
