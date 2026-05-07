651dfdc4543c2f2256b18b25bf691ec8b92053da
viethoang1508 <15082004qwerty@gmail.com>
2026-05-07 09:20:21 +0700

    .

--- book_service/src/main/java/org/example/book_service/mapper/BookMapper.java
+++ book_service/src/main/java/org/example/book_service/mapper/BookMapper.java
@@ -3,6 +3,7 @@ package org.example.book_service.mapper;
 import org.example.book_service.dto.response.InternalBookDTO;
 import org.example.book_service.entity.Book;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
