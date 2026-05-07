8e8be8d5a4a071428f551c3d561f4269e3a66ae4
viethoang1508 <15082004qwerty@gmail.com>
2026-05-07 09:53:40 +0700

    .

---
+++ book_service/src/main/java/org/example/book_service/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package org.example.book_service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedissonClient redissonClient() {
+
+        Config config = new Config();
+
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort);
+
+        return Redisson.create(config);
+    }
+}
